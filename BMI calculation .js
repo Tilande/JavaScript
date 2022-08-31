331c2b757e87b7a4e356af1fc35982523f5abd4c
Tilande <103416237+Tilande@users.noreply.github.com>
2022-09-01 02:05:52 +0700

    Update BMI calculation .js

--- BMI calculation .js
+++ BMI calculation .js
@@ -19,6 +19,6 @@ const mike = {
 }
 }
 
-let whoHasMoreMBI = jack.calBMI() > mike.calBMI() ? `BMI ${jack.name} ${jack.lastname} (${jack.calBMI()}) is higher than ${mike.name} ${mike.lastname} (${mike.calBMI()}).` : `BMI ${mike.name} ${mike.lastname} (${mike.calBMI()}) is higher than ${jack.name} ${jack.lastname} (${jack.calBMI()}).`
+let whoHasAHigherMBI = jack.calBMI() > mike.calBMI() ? `BMI ${jack.name} ${jack.lastname} (${jack.calBMI()}) is higher than ${mike.name} ${mike.lastname} (${mike.calBMI()}).` : `BMI ${mike.name} ${mike.lastname} (${mike.calBMI()}) is higher than ${jack.name} ${jack.lastname} (${jack.calBMI()}).`
 
-console.log(whoHasMoreMBI)
+console.log(whoHasAHigherMBI)
